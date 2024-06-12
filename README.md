be8695918bab5f5b71cc1b29fbbe5e15b626782f
Prem <prem_udayan@yahoo.com>
2024-06-12 12:52:49 -0400

    Updated styles and layout for responsive design

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 # npm install react-cookie
 # npm install react-icons
 # npm install react-circular-progressbar
+
+npm install gh-pages --save-dev
+
+# Add the following 5 lines in package.json
+"homepage": "https://<username>.github.io/<repository>",
+"scripts": {
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d build"
+}
+
+npm run deploy
+
+loging to git and click + in the right corner and add "retirementclock"
+
+git init
+git add .
+git commit -m 'Initial commit'
+git remote add origin https://github.com/premudayan/retirementclock.git
+git remote -v   ///verify remore url
+
+git branch     /// check what is the default branch and update the brach below
+git push -u origin master    /// This will ask you to authenticate
+
+
+Deployment Example for Netlify
+    Create Account: Sign up at Netlify.
+    Connect Repository: Link your GitHub repository.
+    Set Build Command: Use npm run build and set the publish directory to build.
+    Deploy: Netlify will handle the build and deployment automatically. Your app will be live on a free subdomain or a custom domain if you configure one.
--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -33,29 +33,29 @@ const DateInput = ({ defaultDate, defaultHeading, onDateSet, onHeadingSet, onSav
   };
 
   return (
-    <div style={{ textAlign: 'left', margin: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-      <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
-        <label htmlFor="heading" style={{ marginRight: '10px', minWidth: '150px', fontSize: '1rem', color: '#fff' }}>Enter Heading:</label>
+    <div style={{ textAlign: 'left', margin: '20px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+      <div style={{ marginBottom: '10px', display: 'flex', alignItems: 'center', width: '100%' }}>
+        <label htmlFor="heading" style={{ flexBasis: '30%', fontSize: '1.5rem', color: '#fff' }}>Enter Heading:</label>
         <input 
           id="heading"
           type="text" 
           placeholder="Enter heading" 
           value={heading} 
           onChange={handleHeadingChange} 
-          style={{ padding: '5px' }}
+          style={{ flex: '1', padding: '10px', fontSize: '1.2rem' }}
         />
       </div>
-      <div style={{ marginBottom: '20px', display: 'flex', alignItems: 'center' }}>
-        <label htmlFor="date" style={{ marginRight: '10px', minWidth: '150px', fontSize: '1rem', color: '#fff' }}>Enter Target Date:</label>
+      <div style={{ marginBottom: '20px', display: 'flex', alignItems: 'center', width: '100%' }}>
+        <label htmlFor="date" style={{ flexBasis: '30%', fontSize: '1.5rem', color: '#fff' }}>Enter Target Date:</label>
         <input 
           id="date"
           type="date" 
           value={date} 
           onChange={handleDateChange} 
-          style={{ padding: '5px' }}
+          style={{ flex: '1', padding: '10px', fontSize: '1.2rem' }}
         />
       </div>
-      <button onClick={handleSave} style={{ padding: '5px 10px', color: 'black', backgroundColor: '#3e98c7', border: 'none', borderRadius: '5px' }}>
+      <button onClick={handleSave} style={{ padding: '15px 20px', fontSize: '1.5rem', color: 'black', backgroundColor: '#3e98c7', border: 'none', borderRadius: '5px', width: '100%' }}>
         Save
       </button>
     </div>
